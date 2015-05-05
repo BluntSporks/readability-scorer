849d61c1b2d06cd7f450d9683cc03d15ff887b2a
Douglas Green <bluntsporks+git@gmail.com>
2015-05-05 06:07:37 -0400

    Adding Gunning fog index

--- scoreread.go
+++ scoreread.go
@@ -13,17 +13,19 @@ func main() {
 	useAri := flag.Bool("ari", false, "Use Automated Readbility Index (ARI)")
 	useCli := flag.Bool("cli", false, "Use Coleman-Liau Index (CLI)")
 	useFk := flag.Bool("fk", false, "Use Flesch-Kincaid Grade Level")
+	useGfi := flag.Bool("gfi", false, "Use Gunning fog index")
 	flag.Parse()
 
 	// Check arguments.
 	if *file == "" {
 		log.Fatal("Missing -file argument")
 	}
-	if !*useAri && !*useCli && !*useFk {
+	if !*useAri && !*useCli && !*useFk && !*useGfi {
 		// Assume all tests are requested if no tests are requested.
 		*useAri = true
 		*useCli = true
 		*useFk = true
+		*useGfi = true
 	}
 
 	bytes, err := ioutil.ReadFile(*file)
@@ -43,9 +45,16 @@ func main() {
 		cliScore := read.Cli(text)
 		fmt.Printf("Coleman-Liau: %0.2f\n", cliScore)
 	}
+
 	// Score Flesch-Kincaid, if requested.
 	if *useFk {
 		fkScore := read.Fk(text)
 		fmt.Printf("Flesch-Kincaid: %0.2f\n", fkScore)
+	}
+
+	// Score Gunning fog, if requested.
+	if *useGfi {
+		gfiScore := read.Gfi(text)
+		fmt.Printf("Gunning fog: %0.2f\n", gfiScore)
 	}
 }
