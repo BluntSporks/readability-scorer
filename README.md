c7cc615bf7ad89e19b132d4c3750afd0ca95d999
Douglas Green <bluntsporks+git@gmail.com>
2015-05-05 06:23:07 -0400

    Adding SMOG and simplifying

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # readability-tests
 Golang program to score readability using the readability package
+
+# Status
+Ready to use.
+
+However, this package has not been extensively tested. Results may differ in small amounts from official scoring of
+these measures and should not be taken as official.
--- scoreread.go
+++ scoreread.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/BluntSporks/calculation"
 	"github.com/BluntSporks/readability"
 	"io/ioutil"
 	"log"
@@ -10,22 +11,11 @@ import (
 
 func main() {
 	file := flag.String("file", "", "Name of file to filter")
-	useAri := flag.Bool("ari", false, "Use Automated Readbility Index (ARI)")
-	useCli := flag.Bool("cli", false, "Use Coleman-Liau Index (CLI)")
-	useFk := flag.Bool("fk", false, "Use Flesch-Kincaid Grade Level")
-	useGfi := flag.Bool("gfi", false, "Use Gunning fog index")
 	flag.Parse()
 
 	// Check arguments.
 	if *file == "" {
 		log.Fatal("Missing -file argument")
-	}
-	if !*useAri && !*useCli && !*useFk && !*useGfi {
-		// Assume all tests are requested if no tests are requested.
-		*useAri = true
-		*useCli = true
-		*useFk = true
-		*useGfi = true
 	}
 
 	bytes, err := ioutil.ReadFile(*file)
@@ -34,27 +24,28 @@ func main() {
 	}
 	text := string(bytes)
 
-	// Score ARI, if requested.
-	if *useAri {
-		ariScore := read.Ari(text)
-		fmt.Printf("Automated Readability: %0.2f\n", ariScore)
-	}
+	// Score ARI.
+	ariScore := read.Ari(text)
+	fmt.Printf("Automated Readability: %0.2f\n", ariScore)
 
-	// Score CLI, if requested.
-	if *useCli {
-		cliScore := read.Cli(text)
-		fmt.Printf("Coleman-Liau: %0.2f\n", cliScore)
-	}
+	// Score CLI.
+	cliScore := read.Cli(text)
+	fmt.Printf("Coleman-Liau: %0.2f\n", cliScore)
 
-	// Score Flesch-Kincaid, if requested.
-	if *useFk {
-		fkScore := read.Fk(text)
-		fmt.Printf("Flesch-Kincaid: %0.2f\n", fkScore)
-	}
+	// Score Flesch-Kincaid.
+	fkScore := read.Fk(text)
+	fmt.Printf("Flesch-Kincaid: %0.2f\n", fkScore)
 
-	// Score Gunning fog, if requested.
-	if *useGfi {
-		gfiScore := read.Gfi(text)
-		fmt.Printf("Gunning fog: %0.2f\n", gfiScore)
-	}
+	// Score Gunning fog.
+	gfiScore := read.Gfi(text)
+	fmt.Printf("Gunning fog: %0.2f\n", gfiScore)
+
+	// Score SMOG.
+	smogScore := read.Smog(text)
+	fmt.Printf("SMOG: %0.2f\n", smogScore)
+
+	// Yield average score.
+	values := []float64{ariScore, cliScore, fkScore, gfiScore, smogScore}
+	avg := calc.Mean(values)
+	fmt.Printf("Average score: %0.2f\n", avg)
 }
